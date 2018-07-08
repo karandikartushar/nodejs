926e139c4837fcbf95b533aa79419340cc74028b
karandikartushar <tusharkarandikar@gmail.com>
2018-07-08 19:45:01 +0530

    Update App.js

--- lifeCycle/App.js
+++ lifeCycle/App.js
@@ -1,1 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Employee from './Employee.js';
+
+class App extends React.Component{
+
+  constructor(){
+    super();
+    console.log('1. App.class consttructor');
+    this.state = {
+      cityName: 'Mumbai'
+    }
+  }
+  componentWillMount(){
+    console.log('2. App.class ComponentWillMount');
+  }
+  componentDidMount(){
+    console.log('4. App.class componentDidMount');
+  }
+  componentWillReceiveProps(){
+    console.log('App.class componentWillReceiveProps');
+  }
+  shouldComponentUpdate(){
+    console.log('App.class shouldComponentUpdate');
+    return true;
+  }
+  componentWillUpdate(){
+    console.log('App.class componentWillUpdate');
+  }
+  componentDidUpdate(){
+    console.log('App.class componentDidUpdate');
+  }
+  render(){
+    return(
+      <div>
+          <h1> 3. App.class render method</h1>
+          {this.state.cityName}
+          <button onClick={()=> {this.setState({cityName:'Indore'})}}> change status</button>
+          <Employee firstName='Tushar' lastName='Karandikar' age='23' city='pune'/>
+      </div>
+    )
+  }
+
+}
+
+
+ReactDOM.render(<App/>, document.getElementById('root'));
 
