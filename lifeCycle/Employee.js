f63071f568565102cdfe531823f4b726fae673b1
karandikartushar <tusharkarandikar@gmail.com>
2018-07-08 19:45:44 +0530

    Update Employee.js

--- lifeCycle/Employee.js
+++ lifeCycle/Employee.js
@@ -1,1 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Employee extends React.Component{
+
+  constructor(props){
+    super(props);
+    console.log('1. Employee.class consttructor');
+  }
+  componentWillMount(){
+    console.log('2. Employee.class ComponentWillMount');
+  }
+  componentDidMount(){
+    console.log('4. Employee.class componentDidMount');
+  }
+  componentWillReceiveProps(){
+    console.log('Employee.class componentWillReceiveProps');
+  }
+  shouldComponentUpdate(){
+    console.log('Employee.class shouldComponentUpdate');
+    return true;
+  }
+  componentWillUpdate(){
+    console.log('Employee.class componentWillUpdate');
+  }
+  componentDidUpdate(){
+    console.log('Employee.class componentDidUpdate');
+  }
+  render(){
+    return(
+      <div>
+        <h1>3. Employee.class render method!!! {this.props.firstName}</h1>
+      </div>
+    )
+  }
+}
 
