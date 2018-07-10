577180c757cdaf2978e1aca106f6c9ece1574ade
karandikartushar <tusharkarandikar@gmail.com>
2018-07-10 09:54:30 +0530

    Update Util.js

--- expBasic-1/Util.js
+++ expBasic-1/Util.js
@@ -1,1 +1,13 @@
+export default function (){
+  console.log('from default function');
+}
+
+export function test1(){
+  console.log('from test1 function');
+}
+
+export function test2(){
+  var data = ['Tushar','Bakul','Aayushi','Arya'];
+  return data;
+}
 
