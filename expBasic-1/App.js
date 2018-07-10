ac82a481c020b38f8aa84b797f9511942ae68b5c
karandikartushar <tusharkarandikar@gmail.com>
2018-07-10 09:53:45 +0530

    Update App.js

--- expBasic-1/App.js
+++ expBasic-1/App.js
@@ -1,1 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import util, {test1,test2} from './Util.js'
 
+class App extends React.Component{
+  constructor(){
+    super();
+    util();
+    test1();
+    var names = test2();
+    this.state = {
+      nameList:names
+    }
+  }
+  render(){
+    return(
+      <div>
+        <h4>wecome to function exp</h4>
+        {
+          this.state.nameList.map((data)=>
+            <h4>{data}</h4>
+          )
+        }
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App/>,document.getElementById("root"));
