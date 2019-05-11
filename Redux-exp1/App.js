5e7a765302fafa7230b30e59e9b35f7ab08bf78e
karandikartushar <tusharkarandikar@gmail.com>
2019-05-12 00:56:36 +0530

    Update App.js

--- Redux-exp1/App.js
+++ Redux-exp1/App.js
@@ -1,1 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Summary from './Summary';
+import {createStore} from 'redux';
+import {combineReducers} from 'redux';
+import EmployeeReducers from './EmployeeReducer';
+import ActiveUserReducer from './EmployeeReducerAction';
+import {Provider} from 'react-redux'
+
+const allReducer = combineReducers({
+	employee: EmployeeReducers,
+	activeUser: ActiveUserReducer
+})
+const store = createStore(allReducer);
+class App extends React.Component{
+	constructor(){
+		super();
+
+	}
+	render(){
+		return(
+			<div>
+				<Provider store={store}>
+        	<Summary/>
+				</Provider>
+			</div>
+		);
+	}
+
+}
+
+ReactDOM.render(<App/>,document.getElementById("root"));
 
