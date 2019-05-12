f643af6bf068803942fc680956a19a4235b8e642
karandikartushar <tusharkarandikar@gmail.com>
2019-05-12 16:18:39 +0530

    Update ContactReducer.js

--- Redux-exp2/reducers/ContactReducer.js
+++ Redux-exp2/reducers/ContactReducer.js
@@ -1,1 +1,15 @@
+export default function(state={},action){
+console.log('from createContacts reducer--->',action);
+  switch (action.type) {
+    case 'CREATE_CONTACT':
+      return[
+        ...state,
+        Object.assign({}, action.payload)];
+      break;
+    case 'REMOVE_CONTACT':
+      return state.filter((contact) => contact.id !== action.id);
+    break;
+  }
+  return state;
+}
 
